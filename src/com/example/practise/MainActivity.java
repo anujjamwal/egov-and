a20a1b9b519ff7ff7faad0508cdca380f81df82f
Mansi Singhal <singhalmansi777@gmail.com>
2013-03-16 16:05:04 +0530

    Mansi/Pulkit Worked on story to report an incident

--- src/com/example/practise/MainActivity.java
+++ src/com/example/practise/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.practise;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -18,5 +21,15 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public void setIncidentType(View v){
+		
+		Button button = (Button) v;
+		System.out.println(button.ge);
+		
+	}
+	
+	
+
 
 }
