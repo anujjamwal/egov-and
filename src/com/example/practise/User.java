96021f56db0dc1508ec4a6ec8571b39197e1b625
apurvagupta <apurvagupta0@gmail.com>
2013-03-17 17:42:23 +0530

    <apurva/vishal>designed UI for user information

---
+++ src/com/example/practise/User.java
@@ -0,0 +1,15 @@
+package com.example.practise;
+
+public class User 
+{
+	String name,contact_no,email;
+  void getInformation(String name)
+  {
+	 this.name=name; 
+  }
+  
+  void postInformation()
+  {
+	  System.out.println(name);
+  }
+}
